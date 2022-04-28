9f3900d0ed02aa96c65ffee4928c8986bbcb2815
kkaa90 <kkaa90@naver.com>
2022-04-28 17:11:44 +0900

    20220428 자유 게시판 목록 수정

--- app/src/main/java/com/e/myapplication/board/FreeBoardActivity.kt
+++ app/src/main/java/com/e/myapplication/board/FreeBoardActivity.kt
@@ -5,12 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -18,9 +19,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.e.myapplication.R
 import com.e.myapplication.TopMenu
 import com.e.myapplication.dataclass.Board
 import com.e.myapplication.dataclass.Boards
@@ -34,14 +44,14 @@ import java.util.ArrayList
 import javax.security.auth.callback.Callback
 
 class FreeBoardActivity : ComponentActivity() {
-    @SuppressLint("UnrememberedMutableState")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val boards = mutableStateListOf<Board>()
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    val boards = mutableStateListOf<Board>()
+
                     ShowFreeBoardList(boards)
                 }
             }
@@ -52,18 +62,34 @@ class FreeBoardActivity : ComponentActivity() {
 @Composable
 fun Greeting6(board: Board) {
     val context = LocalContext.current
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = {
-                val intent = Intent(context, ShowFreeBoardActivity::class.java)
-                intent.putExtra("num", board.brdId)
-                context.startActivity(intent)
-            })
-    ) {
-        Text(text = board.brdTitle)
-        Text(text = board.memNickname)
+    Card(modifier = Modifier
+        .padding(horizontal = 8.dp, vertical = 4.dp)
+        .fillMaxWidth()
+        .clickable(onClick = {
+            val intent = Intent(context, ShowFreeBoardActivity::class.java)
+            intent.putExtra("num", board.brdId)
+            context.startActivity(intent)
+        })
+        .clip(RoundedCornerShape(12.dp))
+        , elevation = 8.dp) {
+        Column() {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Spacer(modifier = Modifier.width(15.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.schumi), contentDescription = "",
+                    modifier = Modifier.size(100.dp)
+                )
+                Spacer(modifier = Modifier.width(15.dp))
+                Column() {
+                    Text(text = board.brdTitle, fontSize = 24.sp, maxLines = 1)
+                    Text(text = board.memNickname, maxLines = 1)
+                    Text(text = board.brdContents, fontSize = 20.sp, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                }
+                Spacer(modifier = Modifier.width(15.dp))
+            }
+        }
     }
+
 
 }
 
@@ -91,11 +117,12 @@ fun ShowFreeBoardList(boards: SnapshotStateList<Board>) {
     })
 
     Scaffold(
-        topBar = { TopMenu(scaffoldState,scope) },
+        topBar = { TopMenu(scaffoldState, scope) },
         floatingActionButton = {
             FloatingActionButton(onClick = {
                 val intent = Intent(context, WriteFreeBoardActivity::class.java)
-                context.startActivity(intent) }) {
+                context.startActivity(intent)
+            }) {
                 Icon(Icons.Filled.Add, contentDescription = "")
             }
         },
@@ -119,10 +146,33 @@ fun ShowFreeBoardList(boards: SnapshotStateList<Board>) {
 
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview6() {
+    val boards = mutableStateListOf<Board>()
+    boards.add(
+        Board(
+            0, "가나다라", "",
+            0, 0, 0, 0, 0, 0, 0, 0, "제목",
+            "", "", 0, "닉네임"
+        )
+    )
+    boards.add(
+        Board(
+            0, "가나다라", "",
+            0, 0, 0, 0, 0, 0, 0, 0, "제목",
+            "", "", 0, "닉네임"
+        )
+    )
+    boards.add(
+        Board(
+            0, "가나다라zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz", "",
+            0, 0, 0, 0, 0, 0, 0, 0, "제목",
+            "", "", 0, "닉네임"
+        )
+    )
     MyApplicationTheme {
-
+        ShowFreeBoardList(boards = boards)
     }
 }
